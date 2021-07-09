0ff510d71e427e7c924dc7e8c063a68ba66b45f0
russellhimes <william.himes@richmond.edu>
2021-07-09 13:36:06 -0400

    just the echo keyword

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<html><h1>Hire Russell</h1></html>
+echo <html><h1>Hire Russell</h1></html>
 
 <html><p>My name is Russell Himes and I am seeking a position as a junior developer.  Please enjoy this website, which I've set up to demonstrate what I've learned over years of self-study and an intensive course with TechElevator's Java bootcamp.</p></html>
