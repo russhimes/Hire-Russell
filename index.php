b3e77e0115292865513396ad748c6cf185d22b56
russellhimes <william.himes@richmond.edu>
2021-07-09 13:23:43 -0400

    Attempting to test HTML through work

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <php?
-	$html = file_get_html("mainpage.html");
+	echo "<html><h1>Hire Russell</h1></html>"
 
-	echo $html;
+echo "<html><p><br>My name is Russell Himes and I am seeking a position as a junior developer.  Please enjoy this website, which I've set up to demonstrate what I've learned over years of self-study and an intensive course with TechElevator's Java bootcamp.</p></html>"
 ?>
