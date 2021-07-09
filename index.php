8d1bf88ae96a5bc5c1c3c0bc3ddff7a194225ca5
russellhimes <william.himes@richmond.edu>
2021-07-09 13:32:51 -0400

    Again trying to fix the mess

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<php?
 echo "<html><h1>Hire Russell</h1></html>
 
-<html><p><br>My name is Russell Himes and I am seeking a position as a junior developer.  Please enjoy this website, which I've set up to demonstrate what I've learned over years of self-study and an intensive course with TechElevator's Java bootcamp.</p></html>"
+<html><p>My name is Russell Himes and I am seeking a position as a junior developer.  Please enjoy this website, which I've set up to demonstrate what I've learned over years of self-study and an intensive course with TechElevator's Java bootcamp.</p></html>"
