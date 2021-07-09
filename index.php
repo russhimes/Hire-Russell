b2823e1d0ff7fdf553b7c753d3f5138b29964bf0
russellhimes <william.himes@richmond.edu>
2021-07-09 13:27:13 -0400

    fixing visible tags?

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <php?
-	echo "<html><h1>Hire Russell</h1></html>"
+echo <html><h1>Hire Russell</h1></html>
 
-echo "<html><p><br>My name is Russell Himes and I am seeking a position as a junior developer.  Please enjoy this website, which I've set up to demonstrate what I've learned over years of self-study and an intensive course with TechElevator's Java bootcamp.</p></html>"
+<html><p><br>My name is Russell Himes and I am seeking a position as a junior developer.  Please enjoy this website, which I've set up to demonstrate what I've learned over years of self-study and an intensive course with TechElevator's Java bootcamp.</p></html>
 ?>
