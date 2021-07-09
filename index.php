ba23bb921a2cb8687436759e3d99a9f1286a41fd
russellhimes <william.himes@richmond.edu>
2021-07-09 13:34:04 -0400

    yet another attempt to fix the formatting

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-echo "<html><h1>Hire Russell</h1></html>
+echo <html><h1>Hire Russell</h1></html>
 
-<html><p>My name is Russell Himes and I am seeking a position as a junior developer.  Please enjoy this website, which I've set up to demonstrate what I've learned over years of self-study and an intensive course with TechElevator's Java bootcamp.</p></html>"
+<html><p>My name is Russell Himes and I am seeking a position as a junior developer.  Please enjoy this website, which I've set up to demonstrate what I've learned over years of self-study and an intensive course with TechElevator's Java bootcamp.</p></html>
