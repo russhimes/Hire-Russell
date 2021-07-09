39aea39cde259022e68473a8a295f21b7752189d
russellhimes <william.himes@richmond.edu>
2021-07-09 14:08:40 -0400

    can we do it as a seperate file?

--- index.php
+++ index.php
@@ -1,3 +1,1 @@
-echo <html><h1>Hire Russell</h1></html>
-
-<html><p>My name is Russell Himes and I am seeking a position as a junior developer.  Please enjoy this website, which I've set up to demonstrate what I've learned over years of self-study and an intensive course with TechElevator's Java bootcamp.</p></html>
+echo readfile("mainpage.html");
